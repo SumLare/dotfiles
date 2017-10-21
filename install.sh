53b865b1c844195c420396301975ceec4458cd15
SumLare <anohin56n@gmail.com>
2017-10-21 20:14:33 +0300

    Install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+dir="$HOME/dotfiles"
+cd $dir
+
+files="gitconfig zpreztorc zshrc vimrc"
+for file in $files; do
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
+
+# Homebrew
+echo "Installing Homebrew and Cask..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install caks
+brew tap caskroom/cask
+
+# Brew install
+echo "Installing with brew..."
+brew update
+brew install git
+brew install zsh
+brew install node
+brew install postgresql
+brew install docker
+
+# Install apps with Cask
+echo "Installing apps with Cask..."
+brew cask install iterm2
+brew cask install google-chrome
+
+# TODO: Install powerline
+
+# Ruby
+rbenv install 2.4.2
+gem install bundler rails
